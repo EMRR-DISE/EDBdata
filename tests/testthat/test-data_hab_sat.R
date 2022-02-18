a7e542ae9d5f31bee90058830267d4d0a226d355
Dave Bosworth <David.Bosworth@water.ca.gov>
2022-02-18 14:48:25 -0800

    Improving unit test to check for expected period of record of HAB satellite data set

--- tests/testthat/test-data_hab_sat.R
+++ tests/testthat/test-data_hab_sat.R
@@ -55,25 +55,46 @@ test_that("Date is formatted correctly", {
   )))
 })
 
-test_that("Data is present for years 2020 and 2021", {
-  hab_sat_fr_mil_t <- hab_sat_fr_mil %>% dplyr::mutate(Year = lubridate::year(Date))
-  expect_equal(sort(unique(hab_sat_fr_mil_t$Year)), c(2020:2021))
+# Provide a vector of polygon names for the two following tests
+polygon_names_check <- c("Franks Tract", "Mildred Island")
+
+test_that("All polygon Names are as expected", {
+  expect_equal(sort(unique(hab_sat_fr_mil$Name)), polygon_names_check)
 })
 
-test_that("Data is present for months May through December", {
-  hab_sat_fr_mil_t <- hab_sat_fr_mil %>% dplyr::mutate(Month = lubridate::month(Date))
-  expect_equal(sort(unique(hab_sat_fr_mil_t$Month)), c(5:12))
-})
+test_that("Periods of record for each polygon are as expected", {
+  hab_sat_fr_mil_summ <- hab_sat_fr_mil %>%
+    dplyr::mutate(
+      Year = lubridate::year(Date),
+      Month = lubridate::month(Date)
+    ) %>%
+    dplyr::distinct(Name, Year, Month)
 
-test_that("All Names are as expected", {
-  names_check <- c("Franks Tract", "Mildred Island")
-  expect_equal(sort(unique(hab_sat_fr_mil$Name)), names_check)
+  month_por <- function(polygon_name, yr_val) {
+    hab_sat_fr_mil_summ %>%
+      dplyr::filter(Name == polygon_name, Year == yr_val) %>%
+      dplyr::pull(Month)
+  }
+
+  expect_all_months <- function(polygon_name2, yr_val2, months_expect) {
+    eval(bquote(expect_equal(month_por(.(polygon_name2), .(yr_val2)), .(months_expect))))
+  }
+
+  # Test both polygons for year 2020
+  # Define expected months for data in 2020
+  months2020_check <- c(5:12)
+  purrr::map(polygon_names_check, expect_all_months, yr_val2 = 2020, months_expect = months2020_check)
+
+  # Test both polygons for year 2021
+  # Define expected months for data in 2021
+  months2021_check <- c(5:10)
+  purrr::map(polygon_names_check, expect_all_months, yr_val2 = 2021, months_expect = months2021_check)
 })
 
 test_that("All pixel count values are in their expected range", {
-  count_min_max <- function(name, test_var, min_max = c("minimum", "maximum")) {
+  count_min_max <- function(polygon_name, test_var, min_max = c("minimum", "maximum")) {
     hab_sat_fr_mil %>%
-      dplyr::filter(Name == name) %>%
+      dplyr::filter(Name == polygon_name) %>%
       {if (min_max == "minimum") {
         dplyr::summarize(., stat_val = min(.data[[test_var]]))
       } else if (min_max == "maximum") {
@@ -92,21 +113,21 @@ test_that("All pixel count values are in their expected range", {
   )
 
   # Test pixel count variables - minimum is greater than or equal to zero
-  expect_min <- function(name2, test_var2) {
-    eval(bquote(expect_gte(count_min_max(.(name2), .(test_var2), "minimum"), 0)))
+  expect_min <- function(polygon_name2, test_var2) {
+    eval(bquote(expect_gte(count_min_max(.(polygon_name2), .(test_var2), "minimum"), 0)))
   }
-  expect_min_fr <- purrr::partial(expect_min, name2 = "Franks Tract")
+  expect_min_fr <- purrr::partial(expect_min, polygon_name2 = "Franks Tract")
   purrr::map(pixel_vars, expect_min_fr)
-  expect_min_mil <- purrr::partial(expect_min, name2 = "Mildred Island")
+  expect_min_mil <- purrr::partial(expect_min, polygon_name2 = "Mildred Island")
   purrr::map(pixel_vars, expect_min_mil)
 
   # Test pixel count variables - maximum is less than or equal to 166 (Franks) or 42 (Mildred)
-  expect_max <- function(name2, test_var2, test_val) {
-    eval(bquote(expect_lte(count_min_max(.(name2), .(test_var2), "maximum"), .(test_val))))
+  expect_max <- function(polygon_name2, test_var2, test_val) {
+    eval(bquote(expect_lte(count_min_max(.(polygon_name2), .(test_var2), "maximum"), .(test_val))))
   }
-  expect_max_fr <- purrr::partial(expect_max, name2 = "Franks Tract", test_val = 166)
+  expect_max_fr <- purrr::partial(expect_max, polygon_name2 = "Franks Tract", test_val = 166)
   purrr::map(pixel_vars, expect_max_fr)
-  expect_max_mil <- purrr::partial(expect_max, name2 = "Mildred Island", test_val = 42)
+  expect_max_mil <- purrr::partial(expect_max, polygon_name2 = "Mildred Island", test_val = 42)
   purrr::map(pixel_vars, expect_max_mil)
 })
 
