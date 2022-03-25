aeecbd120db90d8b60895f5099bc2590d4837809
Dave Bosworth <David.Bosworth@water.ca.gov>
2022-03-25 11:17:59 -0700

    Adding a crosswalk key for the HAB satellite pixel values to the internal data in the package

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -3,6 +3,8 @@
 
 # Load packages
 library(dplyr)
+library(readr)
+library(stringr)
 library(sf)
 library(here)
 
@@ -13,6 +15,28 @@ i_am("data-raw/sysdata.R")
 sf_edb_reg <- read_sf(here("data-raw/Spatial_data/EDB_Regions.shp")) %>%
   select(Region = Regions)
 
+# Load crosswalk key for the HAB satellite pixel values
+df_hab_sat_pixel_key <- read_csv(here("data-raw/HAB_satellite_data/hab_sat_pixel_val_key.csv")) %>%
+  mutate(
+    # Clean up CyanoIndex and EstimatedMicrocystisCellConc columns
+    across(
+      c(CyanoIndex, EstimatedMicrocystisCellConc),
+      ~ as.numeric(str_remove_all(.x, "^>|^\\?|,"))
+    ),
+    # Make CyanoIndex = 0 at PixelValue = 0
+    CyanoIndex = if_else(PixelValue == 0, 0, CyanoIndex),
+    # Create a column for Cyano Index categories
+    CICategory = case_when(
+      is.na(CyanoIndex) ~ "InvalidOrMissing",
+      CyanoIndex == 0 ~ "NonDetect",
+      CyanoIndex < 2e-04 ~ "Low",
+      CyanoIndex < 1e-03 ~ "Moderate",
+      CyanoIndex < 0.01 ~ "High",
+      TRUE ~ "VeryHigh"
+    )
+  ) %>%
+  select(PixelValue, CyanoIndex, CICategory, EstimatedMicrocystisCellConc, Comments)
+
 # Save internal data set as internal object in the package
-usethis::use_data(sf_edb_reg, internal = TRUE, overwrite = TRUE)
+usethis::use_data(sf_edb_reg, df_hab_sat_pixel_key, internal = TRUE, overwrite = TRUE)
 
